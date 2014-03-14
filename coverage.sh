7512de9d770e37f9fe48b1ade0005f112ab5043b
Alexander Glenn <aglenn@findawayworld.com>
2014-03-14 12:26:57 -0400

    removed travis

--- coverage.sh
+++ coverage.sh
@@ -3,6 +3,7 @@ cd $SRCROOT
 rm -rf gcov
 mkdir gcov
 cp $(find ~/Library/Developer/Xcode/DerivedData/Clicky-fkelmhnxrtegnvgcjslcnkhtwyho/ -name *.gc*) gcov/
+cp $(find ~/Library/Developer/Xcode/DerivedData/Clicky-fkelmhnxrtegnvgcjslcnkhtwyho/ -name *.o) gcov/
 /usr/local/bin/coveralls --exclude lib --exclude tests
 which git > /Users/aglenn/xcode.txt
 which coveralls >> /Users/aglenn/xcode.txt
